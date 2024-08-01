437da8cb2f593484f66ee52d43fd962ebabb4e4a
aluno <aluno@udicentro.local>
2024-08-01 19:37:53 -0300

    criação do spring com bibliotecas

---
+++ prova1Monolitica/provamonoliticagustavo/src/main/java/com/br/provamonoliticagustavo/ProvamonoliticagustavoApplication.java
@@ -0,0 +1,13 @@
+package com.br.provamonoliticagustavo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvamonoliticagustavoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvamonoliticagustavoApplication.class, args);
+	}
+
+}
